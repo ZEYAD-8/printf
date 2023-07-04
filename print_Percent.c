8e785c713a984ac8b81a017fc7243780e4eb745d
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-04 12:52:47 +0300

    task 0

---
+++ print_Percent.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * printPercent - Prints the percent symbol
+ * @args: list of arguments
+ * Return: the amount of characters printed - 1.
+ */
+int printPercent(attribute((unused)) va_list args)
+{
+return (_putchar('%'));
+}
