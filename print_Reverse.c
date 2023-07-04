b8ca73681b85ee7ef00e46ce3eda7e99da938bdf
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-04 16:01:31 +0300

    task 13

---
+++ print_Reverse.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * printReverse - prints a reversed string
+ * @args: list of arguments
+ * Return: the number of characters printed
+*/
+int printReverse(va_list args)
+{
+  char *strin;
+  int i, printed_chars;
+
+  strin = va_arg(args, char *);
+  printed_chars = 0;
+  for (i = strlen(strin) - 1; i >= 0; i--, printed_chars++)
+  {
+    _putchar(strin[i]);
+    printed_chars++;
+  }
+
+  return (printed_chars);
+}
