ed2f679d74860a627db1f8c3c23413c98e6e889c
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-04 05:32:51 -0700

    Task 3

--- print_HEXA.c
+++ print_HEXA.c
@@ -6,36 +6,37 @@
 */
 int printHEX(va_list args)
 {
-  unsigned int num, copy, *arr;
-  int i, len, j;
-  int normal[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-  char heX[] = "0123456789ABCDEF";
+	unsigned int num, copy, *arr;
+	int i, len, j;
+	int normal[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	char heX[] = "0123456789ABCDEF";
 
-  num = va_arg(args, unsigned int);
-  i = 0;
-  copy = num;
-  while (!(copy <= 0))
-  {
-    copy /= 16;
-    i++;
-  }
+	num = va_arg(args, unsigned int);
+	i = 0;
+	copy = num;
+	while (!(copy <= 0))
+	{
+		copy /= 16;
+		i++;
+	}
 
-  len = i;
-  arr = malloc(len * sizeof(unsigned int));
-  while (i > 0)
-  {
-    i--;
-    arr[i] = num % 16;
-    num /= 16;
-  }
+	len = i;
+	arr = malloc(len * sizeof(unsigned int));
+	while (i > 0)
+	{
+		i--;
+		arr[i] = num % 16;
+		num /= 16;
+	}
 
-  for (i = 0; i < len; i++)
-    for (j = 0; j < 16; j++)
-      if (arr[i] == (unsigned int) normal[j])
-      {
-        _putchar(heX[j]);
-        break;
-      }
-  free(arr);
-  return (len);
+	for (i = 0; i < len; i++)
+		for (j = 0; j < 16; j++)
+			if (arr[i] == (unsigned int) normal[j])
+			{
+				_putchar(heX[j]);
+				break;
+			}
+	free(arr);
+	return (len);
 }
+
--- print_hex.c
+++ print_hex.c
@@ -6,34 +6,35 @@
 */
 int printHex(va_list args)
 {
-  unsigned int num, copy, *arr;
-  int i, len, j;
-  int normal[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-  char hex[] = "0123456789abcdef";
+	unsigned int num, copy, *arr;
+	int i, len, j;
+	int normal[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	char hex[] = "0123456789abcdef";
 
-  num = va_arg(args, unsigned int);
-  i = 0;
-  copy = num;
-  while (copy > 0)
-  {
-    copy /= 16;
-    i++;
-  }
-  len = i;
-  arr = malloc(len * sizeof(unsigned int));
-  while (i > 0)
-  {
-    i--;
-    arr[i] = num % 16;
-    num /= 16;
-  }
-  for (i = 0; i < len; i++)
-    for (j = 0; j < 16; j++)
-      if (arr[i] == (unsigned int) normal[j])
-      {
-        _putchar(hex[j]);
-        break;
-      }
-  free(arr);
-  return (len);
+	num = va_arg(args, unsigned int);
+	i = 0;
+	copy = num;
+	while (copy > 0)
+	{
+		copy /= 16;
+		i++;
+	}
+	len = i;
+	arr = malloc(len * sizeof(unsigned int));
+	while (i > 0)
+	{
+		i--;
+		arr[i] = num % 16;
+		num /= 16;
+	}
+	for (i = 0; i < len; i++)
+		for (j = 0; j < 16; j++)
+			if (arr[i] == (unsigned int) normal[j])
+			{
+				_putchar(hex[j]);
+				break;
+			}
+	free(arr);
+	return (len);
 }
+
