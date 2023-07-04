3740fedc5cc76c50e1bd041c39b730a3d6449418
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-04 05:15:51 -0700

    Task 3

--- print_Octal.c
+++ print_Octal.c
@@ -6,30 +6,31 @@
 */
 int printOctal(va_list args)
 {
-  unsigned int num, copy, *arr;
-  int i, len, j;
+	unsigned int num, copy, *arr;
+	int i, len;
 
-  num = va_arg(args, unsigned int);
-  copy = num;
-  i = 0;
-  while (copy > 0)
-  {
-    copy /= 8;
-    i++;
-  }
-  len = i;
-  arr = malloc(len * sizeof(unsigned int));
+	num = va_arg(args, unsigned int);
+	copy = num;
+	i = 0;
+	while (copy > 0)
+	{
+		copy /= 8;
+		i++;
+	}
+	len = i;
+	arr = malloc(len * sizeof(unsigned int));
 
-  while (i > 0)
-  {
-    i--;
-    arr[i] = num % 8;
-    num /= 8;
-  }
+	while (i > 0)
+	{
+		i--;
+		arr[i] = num % 8;
+		num /= 8;
+	}
 
-  for (i = 0; i < len; i++)
-    _putchar('0' + arr[i]);
+	for (i = 0; i < len; i++)
+		_putchar('0' + arr[i]);
 
-  free(arr);
-  return (len);
+	free(arr);
+	return (len);
 }
+
