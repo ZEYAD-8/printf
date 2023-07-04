407eaa1d6909d871cc51a7070039e2cddde28923
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-04 06:05:19 -0700

    Task 13

--- print_Reverse.c
+++ print_Reverse.c
@@ -6,16 +6,17 @@
 */
 int printReverse(va_list args)
 {
-  char *strin;
-  int i, printed_chars;
+	char *strin;
+	int i, printed_chars;
 
-  strin = va_arg(args, char *);
-  printed_chars = 0;
-  for (i = strlen(strin) - 1; i >= 0; i--, printed_chars++)
-  {
-    _putchar(strin[i]);
-    printed_chars++;
-  }
+	strin = va_arg(args, char *);
+	printed_chars = 0;
+	for (i = strlen(strin) - 1; i >= 0; i--, printed_chars++)
+	{
+		_putchar(strin[i]);
+		printed_chars++;
+	}
 
-  return (printed_chars);
+	return (printed_chars);
 }
+
