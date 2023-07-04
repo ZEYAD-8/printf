e0fd69572edb2bbe289adf8dc32e484f9b99a947
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-04 06:09:08 -0700

    Task 14

--- print_ROT13.c
+++ print_ROT13.c
@@ -6,17 +6,18 @@
 */
 int printROT13(va_list args)
 {
-  char *normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char *rot    = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  char *strin;
-  int len, i, j;
+	char *normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot    = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *strin;
+	int len, i, j;
 
-  strin = va_arg(args, char *);
-  len = strlen(strin);
-  for (i = 0; i < len; i++)
-    for (j = 0; j < 52; j++)
-      if (strin[i] == normal[j])
-        _putchar(rot[j]);
+	strin = va_arg(args, char *);
+	len = strlen(strin);
+	for (i = 0; i < len; i++)
+		for (j = 0; j < 52; j++)
+			if (strin[i] == normal[j])
+				_putchar(rot[j]);
 
-  return (len);
+	return (len);
 }
+
